a3037920beda90dbcca76a2e0e6196cbc623e7c8
Shashank <raj.shashank386@gmail.com>
2026-08-16 18:43:33 +0530

    new component created which will have image path field in it

---
+++ core/src/main/java/com/aemcloudproject/core/models/ContentImageModel.java
@@ -0,0 +1,17 @@
+package com.aemcloudproject.core.models;
+
+/**
+ * Sling Model for the Content Image component.
+ */
+public interface ContentImageModel {
+
+    String getTitle();
+
+    String getDescription();
+
+    String getFileReference();
+
+    String getAltText();
+
+    String getImageUrl();
+}
